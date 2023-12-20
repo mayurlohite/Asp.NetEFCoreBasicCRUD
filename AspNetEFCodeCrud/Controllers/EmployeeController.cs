4330a3b0e91e0415fa16af5361303fbafb229b2a
Mayur Lohite <mayee.28@gmail.com>
2023-12-20 06:41:24 +0530

    HTML Layout
    HTML Layout has been added

---
+++ AspNetEFCodeCrud/Controllers/EmployeeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AspNetEFCodeCrud.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
